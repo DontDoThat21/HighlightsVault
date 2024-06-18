eff85185fa74dbdcb1fbffd22bdbe2d9ac5bcbd9
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2024-06-18 17:29:09 -0400

    Add project files.

---
+++ HighlightVault/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HighlightVault.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
