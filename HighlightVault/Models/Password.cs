34ccb463069c24bcd8577f323cd8b984c0055862
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2024-06-20 18:10:47 -0400

    Added Password model.

---
+++ HighlightVault/Models/Password.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HighlightsVault.Models
+{
+    public class Password
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string PasswordValue { get; set; }
+    }
+}
