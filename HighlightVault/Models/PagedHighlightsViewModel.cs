2d269abf60b422cb229d01034be9d1f56b7cfb86
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2024-06-20 18:09:30 -0400

    Added paging (pagination) view model for _HighlightRows partial view.

---
+++ HighlightVault/Models/PagedHighlightsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace HighlightsVault.Models
+{
+    public class PagedHighlightsViewModel
+    {
+        public List<Highlight> Highlights { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }       
+        public int TotalHighlights { get; set; }
+    }
+
+}
